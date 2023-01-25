7e3fa147019af726f3b0473ad6edd52a61afabe2
Arah Barnabas Onuh <arahbarnabasonuh@gmail.com>
2023-01-25 21:37:12 +0100

    jsFundamental2

---
+++ starter/script.js
@@ -0,0 +1,18 @@
+"use strict";
+function logger() {
+    console.log("My name is Barniee");
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcessor(5,0);
